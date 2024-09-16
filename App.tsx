c02b76b10237b6b02d15ad9df6b6c850525c7911
Michael Strahl <michaelstrahl.sm@gmail.com>
2024-09-16 16:59:05 -0300

    add loading

--- App.tsx
+++ App.tsx
@@ -1,12 +1,24 @@
 import { Groups } from '@screens/Groups'
 import { ThemeProvider } from 'styled-components'
+import {
+  useFonts,
+  Roboto_400Regular as Roboto400Regular,
+  Roboto_700Bold as Roboto700Regular,
+} from '@expo-google-fonts/roboto'
+
+import { Loading } from '@components/Loading'
 
 import theme from './src/theme'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto400Regular,
+    Roboto700Regular,
+  })
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   )
 }
