c304fd4b725a0b534b8cf5d9691be0176df0c25a
Michael Strahl <michaelstrahl.sm@gmail.com>
2024-09-16 16:44:21 -0300

    config theme colors and linter

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
-import { Groups } from '@screens/Groups';
-import { ThemeProvider } from 'styled-components';
+import { Groups } from '@screens/Groups'
+import { ThemeProvider } from 'styled-components'
 
 import theme from './src/theme'
 
@@ -8,5 +8,5 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <Groups />
     </ThemeProvider>
-  );
+  )
 }
