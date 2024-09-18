94b467b31af4ffeeb1cded779b86c8adeaff0282
Michael Strahl <michaelstrahl.sm@gmail.com>
2024-09-18 14:38:28 -0300

    finish player card

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -6,10 +6,13 @@ import { Input } from '@components/Input'
 import { Filter } from '@components/Filter'
 import { FlatList } from 'react-native'
 import { useState } from 'react'
+import { PlayerCard } from '@components/PlayerCard'
+import { ListEmpty } from '@components/ListEmpty'
+import { Button } from '@components/Button'
 
 export function Players() {
   const [team, setTeam] = useState('Time A')
-  const [players, setPlayers] = useState<string[]>([])
+  const [players, setPlayers] = useState<string[]>(['Michael', 'Vini'])
 
   return (
     <Container>
@@ -39,6 +42,24 @@ export function Players() {
         />
         <NumberOfPlayers>{players.length}</NumberOfPlayers>
       </HeaderList>
+
+      <FlatList
+        data={players}
+        keyExtractor={(item) => item}
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          players.length === 0 && { flex: 1 },
+        ]}
+        showsVerticalScrollIndicator={false}
+        renderItem={({ item }) => (
+          <PlayerCard name={item} onRemove={() => console.log('removi')} />
+        )}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Não há pessoas nesse time" />
+        )}
+      />
+
+      <Button title="Remover turma" type="secondary" />
     </Container>
   )
 }
